2f0a11cab31e3fae0eba45ee510f0ed2cc5b11b6
tom <thomas.leumann@my.jcu.edu.au>
2022-04-07 19:19:28 +0200

    create minecell component make grid responsive

---
+++ src/components/MineCell.vue
@@ -0,0 +1,31 @@
+<template >
+  <div class="select-none aspect-square flex justify-center items-center border-2 border-gray-500">
+    <span class="text-black ">{{ number }}</span>
+  </div>
+</template>
+<script>
+import useMinefield from '@/stores/minefield'
+export default {
+  name: "MineCell",
+  setup() {
+    const minefield = useMinefield();
+    return { minefield }
+  },
+  props: {
+    index: {
+      type: Number,
+      required: true,
+    }
+  },
+  computed: {
+    number() {
+      return this.minefield.flatField[this.index];
+    }
+  }
+
+  
+}
+</script>
+<style scoped>
+  
+</style>
--- src/components/MineSweeper.vue
+++ src/components/MineSweeper.vue
@@ -1,16 +1,22 @@
 <template>
-  <div class="flex flex-col justify-center">
-    <div class="self-center w-full max-w-[90vh]  p-4 pb-0">
+  <div class="flex flex-col items-center h-[100vh] overflow-hidden">
+    <div class="self-center h-[10vh] p-4 pb-0">
     </div>
-    <div :class="gridColumns" class="self-center w-full max-w-[90vh] grid gap-0 p-4 ">
-
+    <div :class="gridColumns" class="w-[calc(90vh/1.5)] gap-0 p-4">
+      <MineCell 
+        v-for="(number, index) in minefield.flatField"
+        :key="index"
+        :index="index"
+      />
     </div>
   </div>
 </template>
 <script>
-import useMinefield from '@/stores/minefield'
+import useMinefield from '@/stores/minefield';
+import MineCell from '@/components/MineCell.vue'
 export default {
   name: "MineSweeper",
+  components: { MineCell },
   setup() {
     const minefield = useMinefield();
     minefield.newField();
@@ -18,7 +24,8 @@ export default {
   },
   computed: {
     gridColumns() {
-      return `grid-cols-${this.minefield.field[0]?.length}`;
+      let colLength = this.minefield.field[0]?.length;
+      return `grid grid-cols-${colLength}`;
     }
   },
   mounted() {
@@ -27,5 +34,5 @@ export default {
 }
 </script>
 <style scoped>
-  
+
 </style>
